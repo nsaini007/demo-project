bad5445479b76b0fd270bb6c5e42984d0bb8e31a
nsaini007 <nsaini@gammastack.com>
2023-03-21 13:27:33 +0530

    commit 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,18 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'localhost',
+  #   user_name:            '',
+  #   password:             '',
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true,
+  #   open_timeout:         5,
+  #   read_timeout:         5 }
 end
