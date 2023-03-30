ee0ca45949ef828fa8e5767be0a0583507c8ad17
nsaini007 <nsaini@gammastack.com>
2023-03-30 18:01:38 +0530

    30 March 3

--- app/controllers/recruiters/recruiter_applications_controller.rb
+++ app/controllers/recruiters/recruiter_applications_controller.rb
@@ -2,4 +2,6 @@ class Recruiters::RecruiterApplicationsController < Recruiters::BaseController
   def index
     @recruiter_applications = current_user.jobs.find(params[:id]).applications
   end
+
+  
 end
